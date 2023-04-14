76d2b51ebd30d7efdabe4e3084634bb344cb5a03
Almas Orazgaliev <Almas Orazgaliev>
2023-04-14 23:15:37 +0600

    updated view, models, serializers, urls

--- Backend/api/models/disciplines.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from organization import Organization
-
-
-class Disciplines(models.Model):
-    org = models.ForeignKey(Organization, on_delete=models.CASCADE)
-    tutor = models.IntegerField()
-    name = models.CharField(max_length=50)
--- Backend/api/models/room.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Room(models.Model):
-    room_number = models.CharField(max_length=50)
-    capacity = models.IntegerField()
-    organization_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
